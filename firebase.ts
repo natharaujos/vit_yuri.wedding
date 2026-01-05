ceea897a48252d7e790771e1b3814b66660bd1e7
Nathan Araujo <nathansaraujo191@gmail.com>
2026-01-05 18:09:38 -0300

    adjusting firebase file

--- firebase.ts
+++ firebase.ts
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -22,6 +21,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
-const analytics = getAnalytics(app);
 
-export { app, db, auth, analytics };
+export { app, db, auth };
